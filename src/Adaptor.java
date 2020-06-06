07b3ef3ab3fa39e110f2f1e994098aded1b6a52f
NitinK5 <NitinK5@USL-0989.kpit.com>
2020-06-06 19:11:57 -0400

    MultiClassCheck

---
+++ src/Adaptor.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class Adaptor {
+
+	static int p(int x) {
+		return (x + x + x);
+	}
+
+	public static void main(String[] args) throws IOException {
+		if(p(1) != 3)
+            System.out.println("Same!");
+        else
+            System.out.println("Not the Same");
+
+		Messager obj = new Messager();
+		obj.DisplayMessage(p(4));
+		obj.ReadFile();
+	}
+
+}
---
+++ src/Messager.java
@@ -0,0 +1,18 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Messager {
+	public void DisplayMessage(int text) {
+		System.out.print("Check " + text + " now!");
+	}
+
+	public void ReadFile() throws IOException
+	{
+		FileReader fpReader = new FileReader("sample.txt");
+		char [] a = new char[50];
+		fpReader.read();
+		for (char c : a) {
+			System.out.print(c);			
+		}
+	}
+}
