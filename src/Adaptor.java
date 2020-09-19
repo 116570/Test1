c71177682bbdca32b78c25f542f6ba560f588411
116570 <65472146+116570@users.noreply.github.com>
2020-09-18 20:20:34 -0400

    Update Adaptor.java

--- src/Adaptor.java
+++ src/Adaptor.java
@@ -12,7 +12,7 @@ public class Adaptor {
         else
             System.out.println("Not the Same");
 
-		Messager obj1 = new Messager();
+		Messager obj = new Messager();
 		obj.DisplayMessage(p(4));
 		obj.ReadFile();
 	}
